cf8b4316c352741ca430d0d7c806f63d1b30c7c4
Dmitriy <room083@gmail.com>
2025-02-20 21:56:48 +0300

    init

--- script.js
+++ script.js
@@ -30,6 +30,56 @@ function startDiagnosis(issueType) {
     }
 }
 
+function handlePrinterError(answer) {
+    const diagnosisContent = document.getElementById('diagnosisContent')
+    if (answer === 'no') {
+        diagnosisContent.innerHTML = `
+        <p>Вставлена ли бумага для принтера чеков?</p>
+            <button onclick="handleNetworkError('noPaper')">Нет</button>
+            <button onclick="handleNetworkError('yesPaper')">Да</button>
+        <p></p>
+        `;
+    } else if (answer === 'yesPaper') {
+        diagnosisContent.innerHTML = `
+        <p>Крышка отсека бумаги закрыта и горит зеленый индикатор?</p>
+            <button onclick="handleNetworkError('noOpenBox')">Нет</button>
+            <button onclick="handleNetworkError('yesOpenBox')">Да</button>
+        `;
+    } else if (answer === 'noOpenBox') {
+        diagnosisContent.innerHTML = `
+        <p>Попробуйте открыть отсек, заново положить бумагу для принтера в разы отсека и аккуратно закрыть до щелчка.</p>
+        <p>Попробуйте открыть отсек, заново положить бумагу для принтера в разы отсека и аккуратно закрыть до щелчка.</p>
+            <button onclick="handleNetworkError('noOpenBox')">Нет</button>
+            <button onclick="handleNetworkError('yesOpenBox')">Да</button>
+        `;
+    } else if (answer === 'yesOpenBox') {
+        diagnosisContent.innerHTML = `
+        <p>Откройте приложение на главном экране "Настройки". Выберете пункт "Диагностика принтера".</p>
+        <p>Принтер распечатал тестовый чек для диагностики?</p>
+            <button onclick="handleNetworkError('noProblemSolved')">Нет</button>
+            <button onclick="handleNetworkError('yesTestReceipt')">Да</button>
+        `;
+    } else if (answer === 'yesTestReceipt') {
+        diagnosisContent.innerHTML = `
+        <p>Изображение и текст имеют дефекты?</p>
+            <button onclick="handleNetworkError('noImageIssues')">Нет</button>
+            <button onclick="handleNetworkError('yesImageIssues')">Да</button>
+        `;
+    } else if (answer === 'noImageIssues') {
+        diagnosisContent.innerHTML = `
+        <p>Неисправность принтера устранена?</p>
+            <button onclick="handleNetworkError('noProblemSolved')">Нет</button>
+            <button onclick="resetToMainMenu()">Да</button>
+        `;
+    } else if (answer === 'noProblemSolved') {
+        diagnosisContent.innerHTML = `
+            <p>Просьба с данной неисправностью обратиться в сервисный центр.</p>
+            <a href="https://evotor.ru/pokupka-servis/#service" target="_blank">Список сервисных центров</a>
+            <button onclick="resetToMainMenu()">Вернуться</button>
+        `;
+    }
+}
+
 // Обработка проблемы с питанием
 function handlePowerIssue(answer) {
     const diagnosisContent = document.getElementById('diagnosisContent');
@@ -44,8 +94,8 @@ function handlePowerIssue(answer) {
     } else if (answer === 'yesPower') {
         diagnosisContent.innerHTML = `
             <p>Горит ли световой индикатор на устройстве?</p>
-            <button onclick="handlePowerIssue('indicatorOn')">Да</button>
             <button onclick="handlePowerIssue('indicatorOff')">Нет</button>
+            <button onclick="handlePowerIssue('indicatorOn')">Да</button>
         `;
     } else if (answer === 'indicatorOn') {
         diagnosisContent.innerHTML = `
