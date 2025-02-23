4d95554b76a64ff0fab022c5126c3089163f3f04
Dmitriy <room083@gmail.com>
2025-02-23 13:36:15 +0300

    init

--- script.js
+++ script.js
@@ -27,48 +27,87 @@ function startDiagnosis(issueType) {
             <button onclick="handleNetworkError('noWifi')">Нет</button>
             <button onclick="handleNetworkError('yesWifi')">Да</button>
         `;
+    } else if (issueType === 'printerError') {
+        diagnosisTitle.textContent = 'Проблема cо встроенным принтером чеков';
+        diagnosisContent.innerHTML = `
+            <p>Вставлена ли бумага для принтера чеков?</p>
+            <button onclick="handlePrinterError('noPaper')">Нет</button>
+            <button onclick="handlePrinterError('yesPaper')">Да</button>
+        `;
+    } else if (issueType === 'scannerError') {
+        diagnosisTitle.textContent = 'Проблема cо сканером штрих-кодов';
+        diagnosisContent.innerHTML = `
+            <p>При нажатии кнопки сканера и наведении на штрих-код терминал издает звуковой сигнал?</p>
+            <button onclick="handleScannerError('noScan')">Нет</button>
+            <button onclick="handleScannerError('yesScan')">Да</button>
+        `;
+    }
+}
+
+function handleScannerError(answer) {
+    const diagnosisContent = document.getElementById('diagnosisContent')
+    if (answer === 'noScan') {
+        diagnosisContent.innerHTML = `
+        <p>Откройте приложение "Настройки". Выберете пункт "Диагностика сканера штрих-кода".</p>
+        <p>После завершения повторите считывание штрих-кода. Проблема устранена?</p>
+            <button onclick="handleScannerError('noDiagnos')">Нет</button>
+            <button onclick="resetToMainMenu()">Да</button>
+        `;
+    } else if (answer === 'noDiagnos') {
+        diagnosisContent.innerHTML = `
+        <p>Протрите датчик блока сканера салфеткой из микрофибры.</p>
+        <p>Повторите считывание штрих-кода. Проблема устранена?</p>
+            <button onclick="handleScannerError('noClean')">Нет</button>
+            <button onclick="resetToMainMenu()">Да</button>
+        `;
+    } else if (answer === 'noClean') {
+        diagnosisContent.innerHTML = `
+        <p>Возможно сканируемый штрих-код имеет дефекты.</p>
+        <p>Попробуйте отсканировать другой штрих-код.</p>
+            <button onclick="handleScannerError('noBadCode')">Все равно не считывается</button>
+            <button onclick="resetToMainMenu()">Вернуться</button>
+        `;
+    } else if (answer === 'noBadCode') {
+        diagnosisContent.innerHTML = `
+        <p>Просьба с данной неисправностью обратиться в сервисный центр.</p>
+            <a href="https://evotor.ru/pokupka-servis/#service" target="_blank">Список сервисных центров</a>
+            <button onclick="resetToMainMenu()">Вернуться</button>
+        `;
     }
 }
 
 function handlePrinterError(answer) {
     const diagnosisContent = document.getElementById('diagnosisContent')
-    if (answer === 'no') {
-        diagnosisContent.innerHTML = `
-        <p>Вставлена ли бумага для принтера чеков?</p>
-            <button onclick="handleNetworkError('noPaper')">Нет</button>
-            <button onclick="handleNetworkError('yesPaper')">Да</button>
-        <p></p>
-        `;
-    } else if (answer === 'yesPaper') {
+    if (answer === 'yesPaper') {
         diagnosisContent.innerHTML = `
         <p>Крышка отсека бумаги закрыта и горит зеленый индикатор?</p>
-            <button onclick="handleNetworkError('noOpenBox')">Нет</button>
-            <button onclick="handleNetworkError('yesOpenBox')">Да</button>
+            <button onclick="handlePrinterError('noOpenBox')">Нет</button>
+            <button onclick="handlePrinterError('yesOpenBox')">Да</button>
         `;
     } else if (answer === 'noOpenBox') {
         diagnosisContent.innerHTML = `
         <p>Попробуйте открыть отсек, заново положить бумагу для принтера в разы отсека и аккуратно закрыть до щелчка.</p>
-        <p>Попробуйте открыть отсек, заново положить бумагу для принтера в разы отсека и аккуратно закрыть до щелчка.</p>
-            <button onclick="handleNetworkError('noOpenBox')">Нет</button>
-            <button onclick="handleNetworkError('yesOpenBox')">Да</button>
+        <p>Повторите печать чека. Проблема устранена?</p>
+            <button onclick="handlePrinterError('noOpenBox')">Нет</button>
+            <button onclick="handlePrinterError('yesOpenBox')">Да</button>
         `;
     } else if (answer === 'yesOpenBox') {
         diagnosisContent.innerHTML = `
         <p>Откройте приложение на главном экране "Настройки". Выберете пункт "Диагностика принтера".</p>
         <p>Принтер распечатал тестовый чек для диагностики?</p>
-            <button onclick="handleNetworkError('noProblemSolved')">Нет</button>
-            <button onclick="handleNetworkError('yesTestReceipt')">Да</button>
+            <button onclick="handlePrinterError('noProblemSolved')">Нет</button>
+            <button onclick="handlePrinterError('yesTestReceipt')">Да</button>
         `;
     } else if (answer === 'yesTestReceipt') {
         diagnosisContent.innerHTML = `
         <p>Изображение и текст имеют дефекты?</p>
-            <button onclick="handleNetworkError('noImageIssues')">Нет</button>
-            <button onclick="handleNetworkError('yesImageIssues')">Да</button>
+            <button onclick="handlePrinterError('noImageIssues')">Нет</button>
+            <button onclick="handlePrinterError('yesImageIssues')">Да</button>
         `;
     } else if (answer === 'noImageIssues') {
         diagnosisContent.innerHTML = `
         <p>Неисправность принтера устранена?</p>
-            <button onclick="handleNetworkError('noProblemSolved')">Нет</button>
+            <button onclick="handlePrinterError('noProblemSolved')">Нет</button>
             <button onclick="resetToMainMenu()">Да</button>
         `;
     } else if (answer === 'noProblemSolved') {
